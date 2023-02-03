904d5605f3c98ae97675a9f309646f5cec41af9e
pradeepvermagh <pradeepverma.gh@gmail.com>
2023-02-03 17:38:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Example 1
+//* Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be same. Some accepted answer are below
+//* same([1,2,3], [4,1,9])  true
+//* same([1,2,3], [1,9])  false
+//* same([1,2,3], [4,4,1])  false (must be of same frequency)
+
