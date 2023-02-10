c3a21f708d9981844b9d51308d0fb739a510fe43
pradeepvermagh <pradeepverma.gh@gmail.com>
2023-02-10 08:48:52 +0530

    sorting part 1 done

--- index.js
+++ index.js
@@ -234,6 +234,116 @@
 // }
 // console.log(search([1, 2, 3, 4, 5, 6], 4));
 
+
+//* Quiz questions
+// Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+// Bonus Constraints:
+// Time: O(N)
+// Space: O(1)
+
+// Sample Input:
+// averagePair([1,2,3],2.5) // true
+// averagePair([1,3,3,5,6,7,10,12,19],8) // true
+// averagePair([-1,0,3,4,5,6], 4.1) // false
+// averagePair([],4) // false
+
+//* solutions
+// function averagePair(arr, num) {
+//   if (arr.length < 2) {
+//     return false;
+//   }
+
+//   var num1 = 0;
+//   var num2 = 1;
+//   var sum = "";
+//   while (num1 < arr.length - 1) {
+//     console.log(num1, num2, sum);
+//     sum = (arr[num1] + arr[num2]) / 2;
+//     if (sum === num) {
+//       return true;
+//     }
+//     if (num2 == arr.length - 1) {
+//       num1++;
+//       num2 = num1;
+//     }
+//     num2++;
+//   }
+//   return false;
+// }
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+
+//* another solution 
+// function averagePair(arr, num){
+//   let start = 0
+//   let end = arr.length-1;
+//   while(start < end){
+//     let avg = (arr[start]+arr[end]) / 2 
+//     if(avg === num) return true;
+//     else if(avg < num) start++
+//     else end--
+//   }
+//   return false;
+// }
+
+//* Quiz questions
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+// Examples:
+// isSubsequence('hello', 'hello world'); // true
+// isSubsequence('sing', 'sting'); // true
+// isSubsequence('abc', 'abracadabra'); // true
+// isSubsequence('abc', 'acb'); // false (order matters)
+//* sloutions(himanshu)
+// function isSubsequence(str1,str2) {
+//       let i = 0
+//     let str3=''
+
+//     for(let j of str2){
+//         if(str1[i] === j){
+//             str3 += j
+//             i++
+//         }
+//         if(str3 === str1){
+//             return true
+//         }
+//     }
+//     return false
+// }
+//* solution pradeep
+// function isSubsequence(st1,st2){
+//   if(st2.length < st1.length) return false
+//   let string = ''
+//   let first = 0
+//   let second = 0
+//   while(first <= st1.length -1){
+//     if(st1[first] === st2[second]){
+//       string = string + st1[first]
+//       if(string === st1) return true
+//       first++
+//       second = first
+//     }else if(second == st2.length -1){
+//       first++
+//       second = first
+//     }else {
+//       second++
+//     }
+//   }
+//   return false
+// }
+//* Instructor solutions
+// function isSubsequence(str1, str2) {
+//   var i = 0;
+//   var j = 0;
+//   if (!str1) return true;
+//   while (j < str2.length) {
+//     if (str2[j] === str1[i]) i++;
+//     if (i === str1.length) return true;
+//     j++;
+//   }
+//   return false;
+// }
+
 // ------------------------------------------------------------------------------------------------------------ //
 // Recursion ==  A process (a function in our case) that calls itself
 // base code in recursion : A situation when the recursion ends. basically jha hum recursion stop kar de ie return of function
@@ -331,3 +441,207 @@
 // For arrays, use methods like slice, the spread operator, and concat that make copies of arrays so you do not mutate them
 // Remember that strings are immutable so you will need to use methods like slice, substr, or substring to make copies of strings
 // To make copies of objects use Object.assign, or the spread operator
+
+//! Just the code of test of udemy
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+// Examples:
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a') // true
+
+// Solution of above question
+// function areThereDuplicates(...values) {
+//   let object = {};
+//   for (let i = 0; i < values.length; i++) {
+//     object[values[i]] = object[values[i]] + 1 || 1;
+//     if(object[values[i]] > 1) return true
+//   }
+//   return false
+// }
+// console.log(areThereDuplicates("a", "b", "c", "a"));
+
+// ------------------------------------------ Searching Alogrithms ------------------------------------- //
+//* Linear Search : We visit one item at a time starting at the beginning moving forward or moving at a set interval one item at a time checking every single thing.
+
+//* Binary Search :
+//Binary search is a much faster form of search
+//Rather than eliminating one element at a time, you can eliminate half of the remaining elements at a time
+//Binary search only works on //!sorted arrays!
+
+// method refer divide and conquer pattern standard process or use below pattern //* log(N) becuse we find the solution in log(n) steps
+// Original Solution
+// function binarySearch(arr, elem) {
+//     var start = 0;
+//     var end = arr.length - 1;
+//     var middle = Math.floor((start + end) / 2);
+//     while(arr[middle] !== elem && start <= end) {
+//         if(elem < arr[middle]){
+//             end = middle - 1;
+//         } else {
+//             start = middle + 1;
+//         }
+//         middle = Math.floor((start + end) / 2);
+//     }
+//     if(arr[middle] === elem){
+//         return middle;
+//     }
+//     return -1;
+// }
+
+// Refactored Version
+// function binarySearch(arr, elem) {
+//     var start = 0;
+//     var end = arr.length - 1;
+//     var middle = Math.floor((start + end) / 2);
+//     while(arr[middle] !== elem && start <= end) {
+//         if(elem < arr[middle]) end = middle - 1;
+//         else start = middle + 1;
+//         middle = Math.floor((start + end) / 2);
+//     }
+//     return arr[middle] === elem ? middle : -1;
+// }
+
+// binarySearch([2,5,6,9,13,15,28,30], 103)
+
+// ----------------------------  Sorting ----------------------------------- //
+// Sorting is the process of rearranging the items in a collection (e.g. an array) so that the items are in some kind of order.
+// Examples
+// Sorting numbers from smallest to largest
+// Sorting names alphabetically
+// Sorting movies based on release year
+// Sorting movies based on revenue
+
+//* Bubble sort : A sorting algorithm where the largest values bubble up to the top or end! It is good for the data which is nearly  sorted then O(1) otherwise it is O(n^2).
+// website for visual : https://visualgo.net/en/sorting?slide=1-1
+
+// for swaping the number
+// ES5
+// function swap(arr, idx1, idx2) {
+//   var temp = arr[idx1];
+//   arr[idx1] = arr[idx2];
+//   arr[idx2] = temp;
+// }
+
+// ES2015
+// const swap = (arr, idx1, idx2) => {
+//   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+// };
+
+//& UNOPTIMIZED VERSION OF BUBBLE SORT
+// function bubbleSort(arr){
+//     for(var i = arr.length; i > 0; i--){
+//       for(var j = 0; j < i - 1; j++){
+//         console.log(arr, arr[j], arr[j+1]);
+//         if(arr[j] > arr[j+1]){
+//           var temp = arr[j];
+//           arr[j] = arr[j+1];
+//           arr[j+1] = temp;
+//         }
+//       }
+//     }
+//     return arr;
+//   }
+
+// ES2015 Version
+//   function bubbleSort(arr) {
+//     const swap = (arr, idx1, idx2) => {
+//       [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//     };
+
+//     for (let i = arr.length; i > 0; i--) {
+//       for (let j = 0; j < i - 1; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//           swap(arr, j, j + 1);
+//         }
+//       }
+//     }
+//     return arr;
+//   }
+
+//   bubbleSort([8,1,2,3,4,5,6,7]);
+
+//& Optimized BubbleSort with noSwaps
+// function bubbleSort(arr){
+//     var noSwaps;
+//     for(var i = arr.length; i > 0; i--){
+//       noSwaps = true;
+//       for(var j = 0; j < i - 1; j++){
+//         if(arr[j] > arr[j+1]){
+//           var temp = arr[j];
+//           arr[j] = arr[j+1];
+//           arr[j+1] = temp;
+//           noSwaps = false;
+//         }
+//       }
+//       if(noSwaps) break;
+//     }
+//     return arr;
+//   }
+
+//   bubbleSort([8,1,2,3,4,5,6,7]);
+
+//* Selection sort : Similar to bubble sort, but instead of first placing large values into sorted position, it places small values into sorted position. Is O(n^2) it is good only when you want to minimize the number of swaps that you are making. Like in bubble sort we are swaping in each comparison but here we are swaping when on loop ends
+
+// website for visual : https://visualgo.net/en/sorting?slide=1-1   click selection sort
+
+//& LEGACY VERSION (non ES2015 syntax)
+// function sselectionSort(arr){
+//   for(var i = 0; i < arr.length; i++){
+//       var lowest = i;
+//       for(var j = i+1; j < arr.length; j++){
+//           if(arr[j] < arr[lowest]){
+//               lowest = j;
+//           }
+//       }
+//       if(i !== lowest){
+//           //SWAP!
+//           var temp = arr[i];
+//           arr[i] = arr[lowest];
+//           arr[lowest] = temp;
+//       }
+//   }
+//   return arr;
+// }
+
+//& ES2015 VERSION
+// function selectionSort(arr) {
+// const swap = (arr, idx1, idx2) =>
+//   ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+// for (let i = 0; i < arr.length; i++) {
+//   let lowest = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[lowest] > arr[j]) {
+//       lowest = j;
+//     }
+//   }
+//   if (i !== lowest) swap(arr, i, lowest);
+// }
+
+// return arr;
+// }
+
+// selectionSort([0,2,34,22,10,19,17]);
+
+//* Insertion sort : algorithm is to consider the list to be divided into two parts: a sorted part, which is built up from left to right, and an unsorted part. The algorithm repeatedly removes an element from the unsorted part of the list, finds the location within the sorted part where that element belongs, and inserts it there. This continues until there are no more elements in the unsorted part. The advantage of insertion sort is that it is very efficient for small lists and partially sorted lists.
+//insertion sort is good when we have the sorted array and the new numes comes in and we have to add it in the ends
+
+//& https://www.toptal.com/developers/sorting-algorithms    sorting animation website
+
+// function insertionSort(arr){
+// 	var currentVal;
+//     for(var i = 1; i < arr.length; i++){
+//         currentVal = arr[i];
+//         for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+//             arr[j+1] = arr[j]
+//         }
+//         arr[j+1] = currentVal;
+//     }
+//     return arr;
+// }
+
+// insertionSort([2,1,9,76,4])
+
+// website for visual : https://visualgo.net/en/sorting?slide=1-1   Click Insertion Sort
+
+
